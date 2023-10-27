75c4ef678d7a9cbcc641d014604146d1e5e997ec
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-10-27 16:19:09 +0200

    add header and footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,68 @@
+<footer class="bg-dark text-white py-4">
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-3">
+            <div class="col">
+                <h4>Lorem</h4>
+                <ul class="list-unstyled">
+                    <li>
+                        Lorem
+                    </li> 
+                    <li>
+                        Lorem
+                    </li> 
+                    <li>
+                        Lorem
+                    </li> 
+                    <li>
+                        Lorem
+                    </li> 
+                    <li>
+                        Lorem
+                    </li> 
+                </ul>
+            </div>
+
+            <div class="col">
+                <h4>Lorem</h4>
+                <ul class="list-unstyled">
+                    <li>
+                        Lorem
+                    </li> 
+                    <li>
+                        Lorem
+                    </li> 
+                    <li>
+                        Lorem
+                    </li> 
+                    <li>
+                        Lorem
+                    </li> 
+                    <li>
+                        Lorem
+                    </li> 
+                </ul>
+            </div>
+
+            <div class="col">
+                <h4>Lorem</h4>
+                <ul class="list-unstyled">
+                    <li>
+                        Lorem
+                    </li> 
+                    <li>
+                        Lorem
+                    </li> 
+                    <li>
+                        Lorem
+                    </li> 
+                    <li>
+                        Lorem
+                    </li> 
+                    <li>
+                        Lorem
+                    </li> 
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
